91e2533c1fb3d9366626419ad2fe7dde889e5443
ridingrails <el@Gustaves-MacBook-Pro.local>
2013-10-15 08:15:15 -0400

    Using ruby csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,16 @@
+require "csv"
 puts "EventManager Initialized."
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
+
 
